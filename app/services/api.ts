f1c9e9dd3e9bb85aebfaab06a543e23a6cc700fe
Duminda Kodagoda <duminda.g.k@gmail.com>
2025-05-13 02:32:45 +0530

    chore: update backend url

--- app/services/api.ts
+++ app/services/api.ts
@@ -7,7 +7,7 @@ import type {
   PaginationParams,
 } from "@/types";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 const api = axios.create({
   baseURL: API_URL,
